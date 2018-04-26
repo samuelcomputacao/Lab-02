8337c518ab9fb4c3ca3680a7a59dac96478e639a
samuelpv <samuelpv@150.165.42.106>
2018-04-26 11:47:06 -0300

    lsp2

--- src/lab2/Aluno.java
+++ src/lab2/Aluno.java
@@ -1,5 +1,6 @@
 package lab2;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -26,6 +27,18 @@ public class Aluno {
 	 */
 	private Set<ContaLaboratorio> laboratorios;
 	
+	/**
+	 * Representa a saúde de um aluno
+	 */
+	private Saude saude;
 	
+	/**
+	 * Constrói um aluno com os valores de disciplinas, cantinas e laboratótios zerados
+	 */
+	public Aluno() {
+		this.disciplinas = new HashSet<Disciplina>();
+		this.cantinas = new HashSet<ContaCantina>();
+		this.laboratorios = new HashSet<ContaLaboratorio>();
+	}
 
 }
